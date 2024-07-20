7d04ad72aab4520688d8b212a82f12291f2740fa
kssreelakshmi <kssreelakshmi2211@gmail.com>
2024-07-20 14:45:21 +0530

    almost finished

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -13,34 +13,28 @@ const Hero = () => {
         })
     },[])
     
-    const truncate = (str,length) =>{
-        if (!str) return "";
-
-        return str.length > length ? str.slice[0,length] + '...' : str
-    }
-
+    
     if(!movie)
         return (
             <>
             <p>fetching movie...</p>
             </>
 );
-    const {title,backdrop_path,release_date,overview} = movie
-
+    
   return (
 
     <div className='w-full h-[550px]  lg:h-[850px]'>
        <div className='w-full h-full'>
             <div className='absolute w-full h-[550px]  lg:h-[850px] bg-gradient-to-r from-black' />
-                <img className='w-full h-full object-cover object-top' src = {createImageUrl(backdrop_path,'original')} alt={title} />
+                <img className='w-full h-full object-cover object-top' src = {createImageUrl(movie.backdrop_path,'original')} alt={movie.title} />
             <div className='absolute w-full top-[10%] lg:top-[25%] p-4 md:p-8'>
-                <h1 className='text-3xl md:text-5xl font-nsans-bold'>{title}</h1>
+                <h1 className='text-3xl md:text-5xl font-nsans-bold'>{movie.title}</h1>
                 <div className='my-8 mb-4'>
-                    <button className='capitalize border bg-gray-300 text-black py-2 px-5 '>Play</button>
-                    <button className='capitalize border bg-gray-300 text-black py-2 px-5 ml-4'>Watch later</button>
+                    <button className='capitalize border bg-gray-300 rounded-md text-black py-2 px-5 '>Play</button>
+                    <button className='capitalize border bg-gray-300 rounded-md text-black py-2 px-5 ml-4'>Watch later</button>
                 </div>
-                <p className='text-gray-400 text-sm'>{release_date}</p>
-                <p className='text-gray-400 w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%]'>{truncate(overview,170)}</p>
+                <p className='text-gray-400 text-sm'>{movie.release_date}</p>
+                <p className='text-gray-400 w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%]'>{movie.overview ?? "Please wait..."}</p>
             </div>
        </div>
     </div>
--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -28,7 +28,7 @@ const Signup = () => {
     <>
     <div className='w-full h-screen'>
     <img className='hidden sm:block opacity-45 absolute w-full h-full object-cover' src="https://assets.nflxext.com/ffe/siteui/vlv3/8728e059-7686-4d2d-a67a-84872bd71025/e90516bd-6925-4341-a6cf-0b9f3d0c140a/IN-en-20240708-POP_SIGNUP_TWO_WEEKS-perspective_WEB_34324b52-d094-482b-8c2a-708dc64c9065_small.jpg" alt="///" />
-    <div className='fixed w-full px-4 py-36 z-20'>
+    <div className='absolute w-full px-4 py-36 z-20'>
     <div className='max-w-[450px] h-[600px] mx-auto bg-black/70 rounded-xl '>
       <div className='max-w-[320px] mx-auto py-16'>
         <h1 className='text-3xl font-nsans-bold'>Sign Up</h1>
